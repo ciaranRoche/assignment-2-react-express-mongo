4e8d27c5be119f353dc9bff81690bd71eba423d6
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-11-16 15:16:19 +0000

    create user added and working in postman

--- server/api/users/users.controller.js
+++ server/api/users/users.controller.js
@@ -1,5 +1,6 @@
 var User = require('../../models/user');
 
+// returns all the users
 exports.index = function(req, res){
   User.find({}, function(err, users) {
     if (err) {
@@ -9,4 +10,15 @@ exports.index = function(req, res){
     }
     res.json(users);
   });
+}
+
+// creates a user
+exports.create = function(req, res){
+  var user = new User(req.body);
+  user.save((err, createdUserObject) => {
+    if(err){
+      res.status(500).send(err)
+    }
+    res.status(200).send(createdUserObject)
+  })
 }
